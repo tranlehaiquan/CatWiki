66a4e2308cc5bef7f18fe0c985168f282a95db11
Q.Tran <tranlehaiquan@gmail.com>
2021-01-24 01:07:21 +0700

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+
+const postCssPlugins = require('./postcss-config.js');
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -25,6 +28,31 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        postCssPlugins: [...postCssPlugins],
+        cssLoaderOptions: {
+          camelCase: false,
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-webfonts',
+      options: {
+        fonts: {
+          google: [{
+            family: 'Montserrat',
+            variants: ['300', '400', '600'],
+            fontDisplay: 'swap',
+          }, ],
+        },
+        formatAgents: {
+          woff: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36',
+          woff2: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36',
+        },
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -9,10 +9,9 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
-import "./layout.css"
+import Header from "../Header"
 
-const Layout = ({ children }) => {
+const Layout: React.FC = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
